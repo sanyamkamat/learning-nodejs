2711ebac8cea3c9a96e25de76ca7d70e47fa049b
sanyamkamat <sanyamkamat@gmail.com>
2014-06-12 16:48:34 +0530

    setting up interval

--- chunking.js
+++ chunking.js
@@ -3,11 +3,10 @@ var http = require("http");
 var server = http.createServer(function(req,res){
 	res.writeHead(200)
 	res.write("Waiting for 5 seconds....\n");
-	setTimeout(function(){
-		res.write("Wait is over....\n");
-		res.write("Done\n");
-		res.end();
-	},5000);
+	setInterval(function(){
+		res.write("Request\n");
+		//res.end();
+	},1000);
 });
 
 server.listen(9000);
