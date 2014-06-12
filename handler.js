1d93db975748063f9b10312ab31929497fce90c6
sanyamkamat <sanyamkamat@gmail.com>
2014-06-12 14:55:25 +0530

    rendering page using readFileSync

--- handler.js
+++ handler.js
@@ -1,11 +1,21 @@
+var fs = require("fs");
+
 exports.handleRequest = function (req,res){
 	console.log(req.url);
 	switch (req.url){
 		case "/":
 			console.log("demo");
+			renderPage("index",res);
 			res.end("correct");
 			break;
 		default:
+			renderPage("error",res);
 			res.end("Incorrect url!");		
 	}	
 };
+
+function renderPage(pageName,res){
+	var contents = fs.readFileSync(pageName+".html","utf-8");
+	res.write(contents);
+	res.end();
+}
