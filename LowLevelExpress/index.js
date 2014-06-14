bfce83215ab20ab4c1f915a65e6a9433f75945c1
sanyamkamat <sanyamkamat@gmail.com>
2014-06-14 11:00:14 +0530

    using router

--- LowLevelExpress/index.js
+++ LowLevelExpress/index.js
@@ -1,7 +1,10 @@
 var express = require("express");
 var app = express();
+var routes = require("./routes/routes");
 
+/*
 app.get("/",function(req,res) {
 	res.send("Thank You");
-});
+});*/
+app.use("/",routes);
 app.listen(3000);
