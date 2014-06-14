6b6f2fbe7baba9b45bc7bc23b02b59447003a689
sanyamkamat <sanyamkamat@gmail.com>
2014-06-14 11:02:49 +0530

    express using consolidate and handlebars

--- LowLevelExpress/index.js
+++ LowLevelExpress/index.js
@@ -1,10 +1,13 @@
 var express = require("express");
-var app = express();
+var cons = require("consolidate");
 var routes = require("./routes/routes");
+var app = express();
 
-/*
-app.get("/",function(req,res) {
-	res.send("Thank You");
-});*/
-app.use("/",routes);
-app.listen(3000);
+app.engine("hbs",cons.handlebars);
+app.set("views","views");
+app.set("view engine","hbs");
+app.use("/", routes);
+
+var server = app.listen(3000, function() {
+	console.log("listening " + server.address().port);
+});
--- LowLevelExpress/routes/routes.js
+++ LowLevelExpress/routes/routes.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req,res) {
-	res.send("Thank you from Routes");
+	res.render("index",{"title": "Hello Handlebars"});
 });
 
 module.exports = router;
